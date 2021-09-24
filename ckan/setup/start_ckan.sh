93106e5f2fc5e61b387e01c4c12bdffdd5030cee
pooya.oa <pooya.oladazimi@tib.eu>
2021-09-24 16:06:01 +0200

    changed nginx conf

--- ckan/setup/start_ckan.sh
+++ ckan/setup/start_ckan.sh
@@ -19,10 +19,10 @@ cd /usr/lib/ckan/default/src/
 # ckanext-multiuploader
 git clone https://github.com/TIBHannover/ckanext-multiuploader.git
 cd ckanext-multiuploader/
-git checkout main
+git checkout master
 pip install -r requirements.txt
 python setup.py develop
-ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.plugins='image_view text_view multiuploader'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader'
 cd /usr/lib/ckan/default/src
 
 
@@ -32,7 +32,7 @@ cd ckanext-Dataset-Reference/
 git checkout main
 pip install -r requirements.txt
 python setup.py develop
-ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.plugins='image_view text_view multiuploader dataset_reference'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference'
 ckan -c /usr/lib/ckan/default/config/ckan.ini db upgrade -p 'dataset_reference'
 cd /usr/lib/ckan/default/src
 
@@ -40,10 +40,21 @@ cd /usr/lib/ckan/default/src
 git clone https://github.com/TIBHannover/ckanext-tif-imageview.git
 cd ckanext-tif-imageview/
 git checkout main
+pip install pillow
+# pip install -r requirements.txt
+python setup.py develop
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference tif_imageview'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.views.default_views=image_view text_view recline_view pdf_view tif_imageview video_view'
+cd /usr/lib/ckan/default/src
+
+# ckanext-sfb-layout
+git clone https://github.com/TIBHannover/ckanext-sfb-layout.git
+cd ckanext-sfb-layout/
+git checkout master
 pip install -r requirements.txt
 python setup.py develop
-ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.plugins='image_view text_view multiuploader dataset_reference tif_imageview'
-ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.views.default_views = 'image_view text_view recline_view pdf_view tif_imageview video_view'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference tif_imageview user_manual feature_image'
+mkdir -p /usr/lib/ckan/default/data/storage/uploads/admin 
 cd /usr/lib/ckan/default/src
 
 
