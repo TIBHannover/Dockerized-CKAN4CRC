10ea47c022772d0dddb79a2fd057855ba05f8a4b
pooya.oa <pooya.oladazimi@tib.eu>
2021-09-24 14:56:24 +0200

    added tif-imageview plugin

--- ckan/setup/start_ckan.sh
+++ ckan/setup/start_ckan.sh
@@ -36,7 +36,15 @@ ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.plugins='image_view 
 ckan -c /usr/lib/ckan/default/config/ckan.ini db upgrade -p 'dataset_reference'
 cd /usr/lib/ckan/default/src
 
-
+# ckanext-tif-imageview
+git clone https://github.com/TIBHannover/ckanext-tif-imageview.git
+cd ckanext-tif-imageview/
+git checkout main
+pip install -r requirements.txt
+python setup.py develop
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.plugins='image_view text_view multiuploader dataset_reference tif_imageview'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini ckan.views.default_views = 'image_view text_view recline_view pdf_view tif_imageview video_view'
+cd /usr/lib/ckan/default/src
 
 
 
