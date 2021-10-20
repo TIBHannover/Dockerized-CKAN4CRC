776f59b82458ccf634383dd60b4b5a2b8c97a6a8
pooya.oa <pooya.oladazimi@tib.eu>
2021-10-20 12:00:38 +0200

    added cancel-dataset-creation plugin

--- ckan/setup/start_ckan.sh
+++ ckan/setup/start_ckan.sh
@@ -49,6 +49,14 @@ pip install -r requirements.txt
 python setup.py develop
 cd /usr/lib/ckan/default/src
 
+# ckanext-Cancel-Dataset-Creation
+git clone https://github.com/TIBHannover/Ckanext-Cancel-Dataset-Creation.git
+cd Ckanext-Cancel-Dataset-Creation/
+git checkout master
+pip install -r requirements.txt
+python setup.py develop
+cd /usr/lib/ckan/default/src
+
 
 # ckanext-sfb-layout
 git clone https://github.com/TIBHannover/ckanext-sfb-layout.git
@@ -61,7 +69,7 @@ cd /usr/lib/ckan/default/src
 
 
 # Enable and configure all plugins
-ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference tif_imageview user_manual feature_image organization_group'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference tif_imageview user_manual feature_image organization_group cancel_dataset_creation'
 ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.views.default_views=image_view text_view recline_view pdf_view tif_imageview video_view'
 ckan -c /usr/lib/ckan/default/config/ckan.ini db upgrade -p 'dataset_reference'
 
