d849d0a316a5f78b3b8e28a9919784996840a384
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-18 09:57:36 +0100

    added custom-dataset-metadata plugin

--- ckan/setup/start_ckan.sh
+++ ckan/setup/start_ckan.sh
@@ -33,6 +33,7 @@ pip install -r requirements.txt
 python setup.py develop
 cd /usr/lib/ckan/default/src
 
+
 # ckanext-tif-imageview
 git clone https://github.com/TIBHannover/ckanext-tif-imageview.git
 cd ckanext-tif-imageview/
@@ -41,6 +42,7 @@ pip install -r requirements.txt
 python setup.py develop
 cd /usr/lib/ckan/default/src
 
+
 # ckanext-organization-group
 git clone https://github.com/TIBHannover/ckanext-organization-group.git
 cd ckanext-organization-group/
@@ -48,6 +50,7 @@ git checkout main
 pip install -r requirements.txt
 python setup.py develop
 cd /usr/lib/ckan/default/src
+
 
 # ckanext-Cancel-Dataset-Creation
 git clone https://github.com/TIBHannover/Ckanext-Cancel-Dataset-Creation.git
@@ -58,18 +61,18 @@ python setup.py develop
 cd /usr/lib/ckan/default/src
 
 
-# ckanext-sfb-layout
-git clone https://github.com/TIBHannover/ckanext-sfb-layout.git
-cd ckanext-sfb-layout/
-git checkout master
+# ckanext-custom-dataset-metadata
+git clone https://github.com/TIBHannover/ckanext-custom-dataset-metadata.git
+cd ckanext-custom-dataset-metadata/
+git checkout main
 pip install -r requirements.txt
 python setup.py develop
-mkdir -p /usr/lib/ckan/default/data/storage/uploads/admin 
+# mkdir -p /usr/lib/ckan/default/data/storage/uploads/admin 
 cd /usr/lib/ckan/default/src
 
 
 # Enable and configure all plugins
-ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference tif_imageview user_manual feature_image organization_group cancel_dataset_creation'
+ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.plugins=image_view text_view multiuploader dataset_reference tif_imageview organization_group cancel_dataset_creation custom_dataset_type'
 ckan config-tool /usr/lib/ckan/default/config/ckan.ini 'ckan.views.default_views=image_view text_view recline_view pdf_view tif_imageview video_view'
 ckan -c /usr/lib/ckan/default/config/ckan.ini db upgrade -p 'dataset_reference'
 
